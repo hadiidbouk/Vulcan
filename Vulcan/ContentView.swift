66fca148d399c1bae2355cee55cce70799a387f6
Hadi Dbouk <hadi@fueled.com>
2022-07-09 23:53:34 +0300

    Initial Commit

---
+++ Vulcan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vulcan
+//
+//  Created by Hadi Dbouk on 09/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vulcan/VulcanApp.swift
@@ -0,0 +1,17 @@
+//
+//  VulcanApp.swift
+//  Vulcan
+//
+//  Created by Hadi Dbouk on 09/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct VulcanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
